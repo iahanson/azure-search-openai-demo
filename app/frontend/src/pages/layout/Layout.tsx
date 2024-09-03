0ed41a11c1c51301ad84789f3b1749f009135185
iahanson <61495821+iahanson@users.noreply.github.com>
2024-09-03 11:15:09 +0100

    remove ask tab, comment out final bicep files

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -9,51 +9,60 @@ import { useLogin } from "../../authConfig";
 import { LoginButton } from "../../components/LoginButton";
 
 const Layout = () => {
-    return (
-        <div className={styles.layout}>
-            <header className={styles.header} role={"banner"}>
-                <div className={styles.headerContainer}>
-                    <Link to="/" className={styles.headerTitleContainer}>
-                        <h3 className={styles.headerTitle}>GPT4 + RBKC Enterprise data</h3>
-                    </Link>
-                    <nav>
-                        <ul className={styles.headerNavList}>
-                            <li>
-                                <NavLink to="/" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
-                                    Chat
-                                </NavLink>
-                            </li>
-                            <li className={styles.headerNavLeftMargin}>
+  return (
+    <div className={styles.layout}>
+      <header className={styles.header} role={"banner"}>
+        <div className={styles.headerContainer}>
+          <Link to="/" className={styles.headerTitleContainer}>
+            <h3 className={styles.headerTitle}>
+              GPT3.5 + RBKC Enterprise data
+            </h3>
+          </Link>
+          <nav>
+            <ul className={styles.headerNavList}>
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive
+                      ? styles.headerNavPageLinkActive
+                      : styles.headerNavPageLink
+                  }
+                >
+                  Chat
+                </NavLink>
+              </li>
+              {/* <li className={styles.headerNavLeftMargin}>
                                 <NavLink to="/qa" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
                                     Ask a question
                                 </NavLink>
-                            </li>
-                            <li className={styles.headerNavLeftMargin}>
-                                <a
-                                    href="https://officesharedservice.sharepoint.com/sites/RBKCProcurementAct2023"
-                                    target={"_blank"}
-                                    title="Procurement SharePoint link"
-                                >
-                                    <img
-                                        src={rbkc_logo}
-                                        alt="RBKC logo"
-                                        // aria-label="Link to github repository"
-                                        width="200px"
-                                        height="40px"
-                                        className={styles.rbkcLogo}
-                                    />
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-                    <h4 className={styles.headerRightText}>Azure OpenAI + AI Search</h4>
-                    {useLogin && <LoginButton />}
-                </div>
-            </header>
-
-            <Outlet />
+                            </li> */}
+              <li className={styles.headerNavLeftMargin}>
+                <a
+                  href="https://officesharedservice.sharepoint.com/sites/RBKCProcurementAct2023"
+                  target={"_blank"}
+                  title="Procurement SharePoint link"
+                >
+                  <img
+                    src={rbkc_logo}
+                    alt="RBKC logo"
+                    // aria-label="Link to github repository"
+                    width="200px"
+                    height="40px"
+                    className={styles.rbkcLogo}
+                  />
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <h4 className={styles.headerRightText}>Azure OpenAI + AI Search</h4>
+          {useLogin && <LoginButton />}
         </div>
-    );
+      </header>
+
+      <Outlet />
+    </div>
+  );
 };
 
 export default Layout;
